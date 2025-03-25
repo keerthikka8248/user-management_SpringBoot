ac6ed63bbca54de4c0d802adc73cc642a4efe564
keerthikka8248 <keerthikka.s2022cse@sece.ac.in>
2025-03-25 10:26:31 +0530

    deleted classroom entity

--- src/main/java/com/example/user/Repository/ClassroomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.Repository;
-
-import com.example.user.Entity.Classroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
-}
